7878429776dac7cb9a90b331ea07a7bced2a8bfa
Andika Pranayoga <praandikayoga@gmail.com>
2024-06-02 21:13:56 +0800

    Create Banner

---
+++ resources/views/components/my-yamaha.blade.php
@@ -0,0 +1,20 @@
+<div class="myyamaha">
+    <div class="row">
+        <div class="col-6">
+            <img src="{{ asset('img/myyamaha.png') }}" class="img-fluid">
+        </div>
+        <div class="col-6">
+            <img src="{{ asset('img/myy.png') }}" class="img-fluid">
+            <span>My Yamaha Motor</span>
+            <p>
+                "My Yamaha Motor" aplikasi yang dibuat untuk semua konsumen yang mempunyai dan mengendarai sepeda motor
+                Yamaha. Aplikasi ini mendukung anda untuk mendapatkan gaya hidup yang lebih menyenangkan dengan sepeda
+                motor Yamaha.
+            </p>
+            <a href="https://play.google.com/store/apps/details?id=jp.co.yamahamotor.myyamahamotor&pli=1"
+                target="_blank">
+                Download Now
+            </a>
+        </div>
+    </div>
+</div>
