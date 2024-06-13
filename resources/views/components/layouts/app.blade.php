667055bfcec4faf97bc16c26117ff00741cd73fd
Andika Pranayoga <praandikayoga@gmail.com>
2024-06-13 16:32:02 +0800

    Update Menu Mobile v=13

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -14,7 +14,7 @@
             rel="stylesheet">
         <link rel="icon" href="{{ asset('img/yamaha-bisma.png') }}" type="image/x-icon"/>
         @stack('before-css')
-        <link rel="stylesheet" href="{{ asset('css/main5.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/main6.css') }}">
         <link rel="stylesheet" href="{{ asset('css/responsive3.css') }}">
         @stack('after-css')
         <script src="https://kit.fontawesome.com/f7d1a663d2.js" crossorigin="anonymous"></script>
