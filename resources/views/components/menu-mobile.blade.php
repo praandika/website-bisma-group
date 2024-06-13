1984ab95c89ba9aa85e68acc9bbaa492f892bc87
Andika Pranayoga <praandikayoga@gmail.com>
2024-06-13 15:45:49 +0800

    Update Menu Mobile v=1

--- resources/views/components/menu-mobile.blade.php
+++ resources/views/components/menu-mobile.blade.php
@@ -1,6 +1,25 @@
 <nav class="mobile">
     <div class="row">
         <div class="col-12 d-flex justify-content-between">
+            <div class="col-4 logo-menu">
+                <img src="{{ asset('img/footer-logo.png') }}" class="img-fluid">
+            </div>
+            <div class="col-4 contact-menu">
+                <a href="{{ url('contact') }}" wire:navigate>
+                    <img src="{{ asset('img/ic-call.png') }}">
+                    <span>Contact</span>
+                </a>
+            </div>
+            <div class="col-4 simulation-menu">
+                <a href="{{ route('simulation') }}">
+                    <img src="{{ asset('img/ic-calculator.png') }}">
+                    <span>Credit Simulation</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 d-flex justify-content-between">
             <a href="{{ route('home') }}">
                 <img src="{{ asset('img/ic-home.png') }}">
                 <div>Home</div>
@@ -12,14 +31,6 @@
             <a href="{{ url('spart/all') }}" wire:navigate>
                 <img src="{{ asset('img/ic-parts.png') }}">
                 <div>Parts</div>
-            </a>
-            <a href="{{ route('simulation') }}">
-                <img src="{{ asset('img/ic-calculator.png') }}">
-                <div>Credit</div>
-            </a>
-            <a href="{{ url('contact') }}" wire:navigate>
-                <img src="{{ asset('img/ic-call.png') }}">
-                <div>Contact</div>
             </a>
             <a href="{{ url('job/all') }}" wire:navigate>
                 <img src="{{ asset('img/ic-vacancy.png') }}">
