bca9b39b31643dee841419597cebc61a7dcdf80f
Andika Pranayoga <praandikayoga@gmail.com>
2024-07-01 11:46:12 +0800

    Update bunga 1.85 on Credit Simulation Feature v=3

--- resources/views/components/credit-simulation.blade.php
+++ resources/views/components/credit-simulation.blade.php
@@ -18,6 +18,7 @@
             <select id="bunga_menurun">
                 <option>- Pilih Bunga -</option>
                 <option value="0.0240">2.4%</option>
+                <option value="0.0185">1.85%</option>
                 <option value="0.0175">1.75%</option>
             </select>
         </div>
