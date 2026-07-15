7134c2b532e9802cb99f349525c524ca5558d1dd
Andika Pranayoga <praandikayoga@gmail.com>
2026-07-15 12:34:17 +0800

    Update Simulasi Kredit

--- resources/views/page/simulation.blade.php
+++ resources/views/page/simulation.blade.php
@@ -34,9 +34,8 @@
                     <label for="bunga">Select Rate</label>
                     <select id="bunga_menurun" name="rate">
                         <option>- Pilih Bunga -</option>
-                        <option value="0.0240" @if(session('rate') == 0.0240) selected @endif>2.4%</option>
-                        <option value="0.0185" @if(session('rate') == 0.0185) selected @endif>1.85%</option>
-                        <option value="0.0175" @if(session('rate') == 0.0175) selected @endif>1.75%</option>
+                        <option value="0.0244" @if(session('rate') == 0.0244) selected @endif>2.44%</option>
+                        <option value="0.0187" @if(session('rate') == 0.0187) selected @endif>1.87%</option>
                     </select>
                 </div>
 
